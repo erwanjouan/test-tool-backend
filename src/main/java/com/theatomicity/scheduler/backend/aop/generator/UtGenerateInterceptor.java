3f60ecf1a193f21a0d6cf464b9068b6875e3449d
Erwan Jouan <erwan.jouan@theatomicity.com>
2026-01-04 21:29:39 +0100

    externalizes aop ut generator

--- src/main/java/com/theatomicity/scheduler/backend/aop/generator/UtGenerateInterceptor.java
+++ src/main/java/com/theatomicity/scheduler/backend/aop/generator/UtGenerateInterceptor.java
@@ -1,7 +1,6 @@
 package com.theatomicity.scheduler.backend.aop.generator;
 
-import com.theatomicity.scheduler.backend.aop.generator.pojo.InterceptedParam;
-import com.theatomicity.scheduler.backend.aop.generator.pojo.MethodExecution;
+import com.theatomicity.aop.ut.generator.core.EntryPointAroundPointcut;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.ProceedingJoinPoint;
@@ -11,8 +10,6 @@ import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
-import java.util.Objects;
-
 @Aspect
 @Component
 @Slf4j
@@ -20,9 +17,7 @@ import java.util.Objects;
 @Profile("UtGenerateInterceptor")
 public class UtGenerateInterceptor {
 
-    private final TestClassGenerator testClassGenerator;
-
-    private final MethodExecutionCache cache;
+    private final EntryPointAroundPointcut entryPointAroundPointcut;
 
     @Pointcut("execution(* com.theatomicity.scheduler.backend..*(..)) && " +
             " !execution(* com.theatomicity.scheduler.backend.aop..*(..)) && " +
@@ -32,23 +27,6 @@ public class UtGenerateInterceptor {
 
     @Around("allMethodsInRootPackage()")
     public Object logAroundServiceMethods(final ProceedingJoinPoint joinPoint) throws Throwable {
-        final MethodExecution methodExecution = MethodExecution.from(joinPoint);
-        log.info("Before MethodExecution: {} {} {}", methodExecution.getClassName(), methodExecution.getName(), methodExecution.getInputParams());
-        final Object result;
-        try {
-            result = joinPoint.proceed();
-            if (Objects.nonNull(result)) {
-                methodExecution.setResult(new InterceptedParam("result", result.getClass(), result));
-            }
-        } catch (final Throwable e) {
-            log.error("Error during execution of method: {} {}", methodExecution.getClassName(), methodExecution.getName(), e);
-            throw e;
-        } finally {
-            log.info("End MethodExecution: {} {} {}", methodExecution.getClassName(), methodExecution.getName(), methodExecution.getResult());
-            methodExecution.setEndTime(System.nanoTime());
-            this.cache.add(methodExecution);
-            this.testClassGenerator.generateUnitTest(methodExecution);
-        }
-        return result;
+        return this.entryPointAroundPointcut.logAroundServiceMethods(joinPoint);
     }
 }
